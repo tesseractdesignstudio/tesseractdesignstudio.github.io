a3d093c52a27d978e881b6163bdcd39def233990
tesseractdesignstudio <119806502+tesseractdesignstudio@users.noreply.github.com>
2023-01-22 20:37:28 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -188,8 +188,8 @@ $(document).ready(function () {
 
         $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
 
-        if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
-            && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
+        if (MD5($('#invite_code').val()) !== 'bae3f06f361ff66aece8d061505c54ae'
+            && MD5($('#invite_code').val()) !== '670b14728ad9902aecba32e22fa4f6bd') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
             $.post('https://script.google.com/macros/s/16pVAsz_PUXefng-D9c0-F0TRnwg8SvoJKais0u-Q6ihDRMDEZj13mD-c/exec', data)
